d57681a9d108aeb80707ac5862cdbdf0451cbd47
Echo <mail@flyce.cn>
2018-05-09 22:42:10 +0800

    v0.0.2

--- app.js
+++ app.js
@@ -154,7 +154,6 @@ function clacLatitudeIndexJ() {
     return LatitudeIndexJ;
 }
 
-
 console.log(clacLatitudeIndexJ())
 function calcLatitude() {
     return dLat*(clacLatitudeIndexJ() + calcRawLatitude());
